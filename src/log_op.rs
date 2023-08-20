91c772fc0a44fb600dd3b11238fe190b6f056df3
mkd <monkeydioude@gmail.com>
2023-08-20 16:40:35 +0200

    add human_date

--- src/log_op.rs
+++ src/log_op.rs
@@ -1,4 +1,9 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use rocket::serde::json::serde_json::{to_string, Result};
 use serde::{Deserialize, Serialize};
+
+use chrono::{DateTime, Utc, NaiveDateTime};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Log {
@@ -6,4 +11,30 @@ pub struct Log {
     pub msg: String,
     #[serde(default)]
     pub date: u128,
+    #[serde(skip_deserializing)]
+    pub human_date: String,
+}
+
+impl Log {
+    pub fn push_log(&mut self) -> Result<String> {
+        self.date = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        to_string(&self)
+    }
+
+    pub fn read_nomalizing(&self) -> Log {
+        Log {
+            msg: self.msg.clone(),
+            date: self.date,
+            human_date: DateTime::<Utc>::from_utc(
+                NaiveDateTime::from_timestamp_opt(
+                    (self.date / 1000) as i64, ((self.date % 1000) * 1000) as u32
+                ).unwrap(),
+                Utc,
+            ).format("%Y-%m-%d %H:%M:%S").to_string(),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@ extern crate rocket;
 pub mod file_op;
 pub mod log_op;
 
-use std::{net::Ipv4Addr, time::{UNIX_EPOCH, SystemTime}};
+use std::net::Ipv4Addr;
 use file_op::read_lines;
 use log_op::Log;
-use rocket::{serde::json::{Json, to_string, from_str}, Build, Config, Rocket, Route};
+use rocket::{serde::json::{Json, from_str}, Build, Config, Rocket, Route};
 
 const LOG_FILE_PATH: &str = "./logs/logs";
 
 #[get("/logchest/logs")]
 fn display_logs() -> Json<Vec<Log>> {
+    let transformer = |line: (usize, &str)| -> Log {
+        let log = from_str::<Log>(line.1).unwrap();
+
+        log.read_nomalizing()
+    };
     Json(
-        match read_lines::<Log>(LOG_FILE_PATH, |line| from_str(line.1).unwrap()) {
+        match read_lines::<Log>(LOG_FILE_PATH, transformer) {
             Ok(res) => res,
             Err(_) => vec!(),
         }
@@ -26,26 +31,18 @@ fn display_logs() -> Json<Vec<Log>> {
 fn add_log(mut log: Json<Log>) -> String {
     let mut fm8kr = file_op::create_file(LOG_FILE_PATH);
     if let Err(res) = fm8kr.with_directories().go_my_dude() {
-        panic!("{}", res);
-    }
-    log.date = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis();
+        return res.to_string();
+    } 
 
-    match to_string(&*log) {
-            Ok(msg) => {
-                if let Err(err) = fm8kr.write_line(&msg) {
-                    return err.to_string()
-                }
-                format!(
-                    "log added msg: {}, date: {}",
-                    log.msg, log.date
-                )
-            },
-            Err(err) => err.to_string()
+    match (*log).push_log() {
+        Ok(msg) => {
+            if let Err(err) = fm8kr.write_line(&msg) {
+                return err.to_string();
+            }
+            format!("log added msg: {}, date: {}", log.msg, log.date)
+        }
+        Err(err) => err.to_string(),
     }
-
 }
 
 fn lezgong(routes: Vec<Route>, port: u16) -> Rocket<Build> {
